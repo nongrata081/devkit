ab92a076e8bcfce8561cdad3716cd676dad7648b
Vahan Minasian <vaanatwork@gmail.com>
2019-04-09 13:01:17 +0200

    feat(commitlint): add commitlint schematics

--- src/commitlint/index.ts
+++ src/commitlint/index.ts
@@ -1,10 +1,76 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, SchematicsException, Tree } from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 
-
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
 export function commitlint(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+
+    const packageJsonBuffer = tree.read("package.json");
+    if (!packageJsonBuffer) {
+      throw new SchematicsException("Not an npm project. Couldn't find package.json");
+    }
+
+    const packageJson = JSON.parse(packageJsonBuffer.toString());
+
+    const devDeps = "devDependencies";
+    const commitlintCliDep = "@commitlint/cli";
+    const configConventionalDep = "@commitlint/config-conventional";
+    const husky = "husky";
+    const hooks = "hooks";
+    const commitMsgHook = "commit-msg";
+    const commitMsgScript = "commitlint -E HUSKY_GIT_PARAMS";
+    const version = "latest";
+    const commitlint = "commitlint";
+    const commitLintextends = "extends";
+
+    if (!packageJson[devDeps]) {
+      packageJson[devDeps] = {};
+    }
+    if (!packageJson[devDeps][husky]) {
+      packageJson[devDeps][husky] = version;
+    }
+    if (!packageJson[devDeps][commitlintCliDep]) {
+      packageJson[devDeps][commitlintCliDep] = version;
+    }
+    if (!packageJson[devDeps][configConventionalDep]) {
+      packageJson[devDeps][configConventionalDep] = version;
+    }
+    if (!packageJson[commitlint]) {
+      packageJson[commitlint] = {};
+      packageJson[commitlint][commitLintextends] = [configConventionalDep];
+    } else {
+      if (!packageJson[commitlint][commitLintextends]) {
+        packageJson[commitlint][commitLintextends] = [configConventionalDep];
+      } else {
+        if (packageJson[commitlint][commitLintextends].indexOf(configConventionalDep) === -1) {
+          _context.logger.log('info', `commit lint extends non-conventional config, rewriting to enforce conventional config`);
+          packageJson[commitlint][commitLintextends] = [configConventionalDep];
+        }
+      }
+    }
+
+    if (!packageJson[husky]) {
+      packageJson[husky] = {};
+      packageJson[husky][hooks] = {};
+      packageJson[husky][hooks][commitMsgHook] = commitMsgScript;
+    } else {
+      if (!packageJson[husky][hooks]) {
+        packageJson[husky][hooks] = {};
+        packageJson[husky][hooks][commitMsgHook] = commitMsgScript;
+      } else {
+        if (packageJson[husky][hooks][commitMsgHook] !== commitMsgScript) {
+          _context.logger.log('info', `husky commit-msg hook is present & different from commitlint, rewriting to enforce commitlint`);
+          packageJson[husky][hooks][commitMsgHook] = commitMsgScript;
+        }
+      }
+    }
+
+    tree.overwrite('package.json', JSON.stringify(packageJson, null, 2));
+    _context.logger.log('info', `Added "${commitlintCliDep}@${version}" to "${devDeps}" in package.json`);
+    _context.logger.log('info', `Added "${configConventionalDep}@${version}" to "${devDeps}" in package.json`);
+    _context.logger.log('info', `Added "${husky}@${version}" to "${devDeps}" in package.json`);
+    _context.logger.log('info', `Added "${commitMsgScript}" to "husky.hooks.commit-msg" in package.json`);
+    _context.addTask(new NodePackageInstallTask());
+
     return tree;
   };
 }
