14c460f1d98e57e00105646714aebd0880cd0a8a
Vahan Minasian <vaanatwork@gmail.com>
2019-04-09 13:21:13 +0200

    feat(commitprompt): added commitprompt schematics

--- src/commitprompt/index.ts
+++ src/commitprompt/index.ts
@@ -1,10 +1,62 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, SchematicsException, Tree } from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 
-
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
 export function commitprompt(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+
+    const packageJsonBuffer = tree.read("package.json");
+    if (!packageJsonBuffer) {
+      throw new SchematicsException("Not an npm project. Couldn't find package.json");
+    }
+
+    const packageJson = JSON.parse(packageJsonBuffer.toString());
+
+    const scripts = "scripts";
+    const commitScript = "commit";
+    const commitScriptCmd = "git-cz";
+    if (!packageJson[scripts]) {
+      packageJson[scripts] = {};
+    }
+    packageJson[scripts][commitScript] = commitScriptCmd;
+
+    const devDeps = "devDependencies";
+    const commitizen = "commitizen";
+    const commitizenConfig = "cz-conventional-changelog";
+    const path = "path";
+    const version = "latest";
+    if (!packageJson[devDeps]) {
+      packageJson[devDeps] = {};
+    }
+    if (!packageJson[devDeps][commitizen]) {
+      packageJson[devDeps][commitizen] = version;
+    }
+    if (!packageJson[devDeps][commitizenConfig]) {
+      packageJson[devDeps][commitizenConfig] = version;
+    }
+
+    const config = "config";
+    if (!packageJson[config]) {
+      packageJson[config] = {};
+      packageJson[config][commitizen] = {};
+      packageJson[config][commitizen][path] = commitizenConfig;
+    } else {
+      if (!packageJson[config][commitizen]) {
+        packageJson[config][commitizen] = {};
+        packageJson[config][commitizen][path] = commitizenConfig;
+      } else {
+        if (packageJson[config][commitizen][path] !== commitizenConfig) {
+          _context.logger.log('info', `commitizen uses a non-conventional config, rewriting to enforce ${commitizenConfig}`);
+          packageJson[config][commitizen][path] = commitizenConfig;
+        }
+      }
+    }
+
+    tree.overwrite('package.json', JSON.stringify(packageJson, null, 2));
+    _context.logger.log('info', `Added "${commitizen}@${version}" to "${devDeps}" in package.json`);
+    _context.logger.log('info', `Added "${commitizenConfig}@${version}" to "${devDeps}" in package.json`);
+    _context.logger.log('info', `Added "${commitizenConfig}" to "config.commitizen.path" in package.json`);
+    _context.addTask(new NodePackageInstallTask());
+
     return tree;
   };
 }
